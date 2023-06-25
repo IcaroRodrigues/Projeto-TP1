45709e942e3cb273b95656c9e3c397a1f2bf053a
Gabrielspg13 <55856404+Gabrielspg13@users.noreply.github.com>
2023-06-25 03:02:53 -0300

    Update Corretor.java
    Adicionei a micro documentação, adicionei o atributo boolean adm ao construtor e inicializei ele como false (pois um corretor nunca vai ser também adm), aproveitei e inicializei a função como "Corretor" e editei alguns comentários!

--- src/Classes/Corretor.java
+++ src/Classes/Corretor.java
@@ -1,28 +1,30 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
 package Classes;
 
 /**
- *
- * @author Gabriel Santos Pereira
- */
+* Classe que representa um corretor.
+* Contém as informações básicas de identificação e contato de uma pessoa derivadas da classe Pessoa, a demais informações de um funcionario derivadas da classe Funcionario e o creci do corretor.
+* 
+* @author Gabriel Santos Pereira
+*/
 public class Corretor extends Funcionario{
-    //Declaração dos Atributos
+    // Declaração dos Atributos
     private String creci;
     
-    //Declaração dos Métodos Construtores
-    //inicialização de todos os atributos com valores default
+    // Declaração dos Métodos Construtores
+    // Inicialização de todos os atributos com valores default
     public Corretor(String creci) {}
 
-    //inicialização recebendo todos os argumentos de acordo com os atributos
-    public Corretor(int id, String nome, String rg, String cpf, String dataNascimento, String telefone, String funcao, float salario, String senha, String creci) {
-        super(id, nome, rg, cpf, dataNascimento, telefone, funcao, salario, senha);
+    // Inicialização recebendo todos os argumentos de acordo com os atributos
+    public Corretor(int id, String nome, String rg, String cpf, String dataNascimento, String telefone, String funcao, float salario, String senha, boolean adm, String creci) {
+        super(id, nome, rg, cpf, dataNascimento, telefone, "Corretor", salario, senha, false);
         this.creci = creci;
     }
     
-    //Declaração dos Métodos gets e sets
+    // Declaração dos Métodos gets e sets
     public String getCreci() {
         return creci;
     }
