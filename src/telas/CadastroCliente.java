f8ecd3ce6737d351fd7c021838a1d7e1da522a21
icaro <icaro@host.docker.internal>
2023-07-05 19:44:03 -0300

    Refatora telas de cadastro

--- src/telas/CadastroCliente.java
+++ src/telas/CadastroCliente.java
@@ -50,8 +50,10 @@ public class CadastroCliente extends javax.swing.JFrame {
         btnVoltarTelaPrincipal = new javax.swing.JButton();
         txtCpfCliente = new javax.swing.JFormattedTextField();
         txtDataDeNascimentoCliente = new javax.swing.JFormattedTextField();
-        txtTelefoneCliente = new javax.swing.JFormattedTextField();
         txtRgCliente = new javax.swing.JFormattedTextField();
+        txtTelefoneCliente = new javax.swing.JFormattedTextField();
+        lblTelefoneCliente1 = new javax.swing.JLabel();
+        txtOcupacaoCliente = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Cadastro de Cliente");
@@ -64,7 +66,7 @@ public class CadastroCliente extends javax.swing.JFrame {
 
         lblDataDeNascimentoCliente.setText("Data de Nascimento:");
 
-        lblTelefoneCliente.setText("Telefone:");
+        lblTelefoneCliente.setText("Ocupação:");
 
         btnSalvarCliente.setText("Salvar");
         btnSalvarCliente.addActionListener(new java.awt.event.ActionListener() {
@@ -105,25 +107,35 @@ public class CadastroCliente extends javax.swing.JFrame {
         });
 
         try {
-            txtTelefoneCliente.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("(##) 9 ####-####")));
+            txtRgCliente.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("#.###.###")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
 
         try {
-            txtRgCliente.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("#.###.###")));
+            txtTelefoneCliente.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("(##) 9 ####-####")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
+
+        lblTelefoneCliente1.setText("Telefone:");
 
         javax.swing.GroupLayout pnlDadosClienteLayout = new javax.swing.GroupLayout(pnlDadosCliente);
         pnlDadosCliente.setLayout(pnlDadosClienteLayout);
         pnlDadosClienteLayout.setHorizontalGroup(
             pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnlDadosClienteLayout.createSequentialGroup()
-                .addGap(0, 100, Short.MAX_VALUE)
-                .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(pnlDadosClienteLayout.createSequentialGroup()
+                .addGap(150, 150, 150)
+                .addComponent(btnSalvarCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(btnLimparCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(btnVoltarTelaPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(146, Short.MAX_VALUE))
+            .addGroup(pnlDadosClienteLayout.createSequentialGroup()
+                .addGap(100, 100, 100)
+                .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDadosClienteLayout.createSequentialGroup()
                         .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDadosClienteLayout.createSequentialGroup()
                                 .addComponent(lblRgCliente)
@@ -137,26 +149,22 @@ public class CadastroCliente extends javax.swing.JFrame {
                             .addComponent(txtNomeCliente)
                             .addComponent(txtCpfCliente, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
                             .addComponent(txtRgCliente)))
-                    .addGroup(pnlDadosClienteLayout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDadosClienteLayout.createSequentialGroup()
                         .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDadosClienteLayout.createSequentialGroup()
-                                .addComponent(lblTelefoneCliente)
-                                .addGap(80, 80, 80))
                             .addGroup(pnlDadosClienteLayout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
                                 .addComponent(lblDataDeNascimentoCliente)
-                                .addGap(15, 15, 15)))
+                                .addGap(20, 20, 20))
+                            .addGroup(pnlDadosClienteLayout.createSequentialGroup()
+                                .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblTelefoneCliente1)
+                                    .addComponent(lblTelefoneCliente))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtTelefoneCliente, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
                             .addComponent(txtDataDeNascimentoCliente)
-                            .addComponent(txtTelefoneCliente, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE))))
+                            .addComponent(txtOcupacaoCliente))))
                 .addGap(100, 100, 100))
-            .addGroup(pnlDadosClienteLayout.createSequentialGroup()
-                .addGap(150, 150, 150)
-                .addComponent(btnSalvarCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20)
-                .addComponent(btnLimparCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20)
-                .addComponent(btnVoltarTelaPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(141, Short.MAX_VALUE))
         );
         pnlDadosClienteLayout.setVerticalGroup(
             pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -179,9 +187,13 @@ public class CadastroCliente extends javax.swing.JFrame {
                     .addComponent(txtDataDeNascimentoCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblTelefoneCliente)
+                    .addComponent(lblTelefoneCliente1)
                     .addComponent(txtTelefoneCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtOcupacaoCliente, javax.swing.GroupLayout.DEFAULT_SIZE, 29, Short.MAX_VALUE)
+                    .addComponent(lblTelefoneCliente))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(pnlDadosClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnLimparCliente)
                     .addComponent(btnVoltarTelaPrincipal)
@@ -199,7 +211,7 @@ public class CadastroCliente extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(65, 65, 65)
                 .addComponent(pnlDadosCliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(65, Short.MAX_VALUE))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         pack();
@@ -247,7 +259,7 @@ public class CadastroCliente extends javax.swing.JFrame {
             String dataNascimentoCliente_ = txtDataDeNascimentoCliente.getText();
             String telefoneCliente_ = txtTelefoneCliente.getText();
 
-            Cliente cliente = new Cliente(nomeCliente_, rgCliente_, cpfCliente_, dataNascimentoCliente_, telefoneCliente_);
+            Cliente cliente = new Cliente(nomeCliente_, rgCliente_, cpfCliente_, dataNascimentoCliente_, telefoneCliente_, telefoneCliente_);
 	    ClienteDAO clienteDAO = new ClienteDAO();
 	    if (clienteDAO.salvar(cliente)){ 
 		JOptionPane.showMessageDialog(null, "Cliente cadastrado com Sucesso!", "Aviso!", JOptionPane.INFORMATION_MESSAGE);
@@ -308,10 +320,12 @@ public class CadastroCliente extends javax.swing.JFrame {
     private javax.swing.JLabel lblNomeCliente;
     private javax.swing.JLabel lblRgCliente;
     private javax.swing.JLabel lblTelefoneCliente;
+    private javax.swing.JLabel lblTelefoneCliente1;
     private javax.swing.JPanel pnlDadosCliente;
     private javax.swing.JFormattedTextField txtCpfCliente;
     private javax.swing.JFormattedTextField txtDataDeNascimentoCliente;
     private javax.swing.JTextField txtNomeCliente;
+    private javax.swing.JTextField txtOcupacaoCliente;
     private javax.swing.JFormattedTextField txtRgCliente;
     private javax.swing.JFormattedTextField txtTelefoneCliente;
     // End of variables declaration//GEN-END:variables
